bbfc1780cdba80e029dbbb474878dac7b618457f
Nkanga Richard <75099089+Nkanga-Coded-It@users.noreply.github.com>
2021-09-14 20:05:17 +0200

    Added animation for splash screen assets(logo and artwork)

--- app/src/main/java/com/example/g_fit/SplashScreen.java
+++ app/src/main/java/com/example/g_fit/SplashScreen.java
@@ -3,12 +3,17 @@ package com.example.g_fit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //To Hide Status Bar (Battery percentage, time e.t.c)
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_splash_screen);
     }
 }
