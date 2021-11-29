3bf7ee51651c05913b2f0a8c6ee75079e569b470
Nkanga Richard <75099089+Nkanga-Coded-It@users.noreply.github.com>
2021-11-29 19:30:37 +0100

    added code for the signup screen and code to store user data

---
+++ app/src/main/java/com/example/g_fit/User.java
@@ -0,0 +1,75 @@
+package com.example.g_fit;
+
+public class User {
+
+    public String email,password, age, name;
+
+
+    public User() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public User(String email, String password, String age, String name) {
+        this.email = email;
+        this.password = password;
+        this.age = age;
+        this.name = name;
+
+
+
+    }
+
+    //    public User(String email, String password) {
+//        this.email = email;
+//        this.password = password;
+//    }
+//
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//
+
+    }
+
